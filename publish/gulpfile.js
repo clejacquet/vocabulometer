5fc9df2faac91a4f3650832c6370bda15442f6d9
clejacquet <clem.jacquet@gmail.com>
2018-08-02 18:36:40 +0900

    Add of heroku sync tasks to publish

--- publish/gulpfile.js
+++ publish/gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
+const gulpSh = require('gulp-shell');
 const fs = require('fs');
+const run = require('run-sequence');
 
 const loadJsonFile = (filename) => {
     return JSON.parse(fs.readFileSync(filename, 'utf8'));
@@ -20,3 +22,15 @@ toProcessArray.forEach(typeItem => {
         typeProcessor(gulp, server);
     });
 });
+
+gulp.task('heroku:install', gulpSh.task('curl https://cli-assets.heroku.com/install.sh | sh'));
+
+gulp.task('heroku:uninstall', gulpSh.task('sudo rm /usr/local/bin/heroku && sudo rm -rf /usr/local/lib/heroku /usr/local/heroku && sudo rm -rf ~/.local/share/heroku ~/.cache/heroku'));
+
+gulp.task('heroku:login', gulpSh.task('heroku login'));
+
+gulp.task('heroku:clone-dev', gulpSh.task('heroku git:clone -a vocabulometer-dev servers/heroku/dev'));
+
+gulp.task('heroku:clone-prod', gulpSh.task('heroku git:clone -a vocabulometer servers/heroku/stable'));
+
+gulp.task('heroku:init', [ 'heroku:clone-dev', 'heroku:clone-prod' ]);
